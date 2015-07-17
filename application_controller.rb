48553979f3157a1f972a3daaca59d65675f123c9
Trevor K <trevor.keig@gmail.com>
2015-07-17 16:22:13 +0000

    Search feedback and centered search results

--- application_controller.rb
+++ application_controller.rb
@@ -14,18 +14,44 @@ class MyApp < Sinatra::Base
   end
   
   post '/imagesort' do
-    categories = params[:category]
-    resolution = params[:resolution]
-    categories.compact
-    resolution.compact
-    sortimages(categories, resolution)
+    @categories = params[:category]
+    @resolution = params[:resolution]
+    @categories.compact
+    @resolution.compact
+    sortimages(@categories, @resolution)
     imagenum = 1
-    finalimages = []
+    @finalimages = []
     $results.each do |image, info|
-      finalimages.push("<a href=#{image} target=_blank> <img src= #{image} width= 160 height= 90></a> <p>Category: #{info.at(0).capitalize}. Resolution: #{info.at(1)}</p>")
+      @finalimages.push("<div id=image align=center> <a href=#{image} target=_blank> <img src= #{image} width= 160 height= 90></a></div> <p>Category: #{info.at(0).capitalize}. Resolution: #{info.at(1)}</p>")
     end
-    return finalimages
+  @categorynumber = "category"
+  @resolutionnumber = "resolution"
+  @categories.each do |category|
+    category.capitalize!
+  end
+  if @categories.length == 1
+    @categories = @categories[0]
+  elsif @categories.length == 2
+    @categorynumber = "categories"
+    @categories = @categories.join(" and ")
+  elsif @categories.length > 2
+    @categorynumber = "categories"
+    @categories[-1] = "and #{@categories[-1]}"
+  @categories = @categories.join(", ")
+  end
+
+  if @resolution.length == 1
+  @resolution = @resolution[0]
+  elsif @resolution.length == 2
+    @resolutionnumber = "resolutions"
+    @resolution = @resolution.join(" and ")
+
+  elsif @resolution.length > 2
+    @resolutionnumber = "resolutions"
+    @resolution[-1] = "and #{@resolution[-1]}"
+    @resolution = @resolution.join(", ")
+  end
     erb :imagesort
   end
-  
+
 end
